88a2a13edf32389b2d6bbb3ee2214b0485c36133
Nancykamala <116730169+Nancykamala@users.noreply.github.com>
2024-04-24 00:19:57 +0530

    Create weatherapp.py

---
+++ weatherapp.py
@@ -0,0 +1,18 @@
+import requests
+
+api_key = '177f3b62addf831447cfdd0482f6fa25'
+
+city = input('Enter city name: ')
+
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    temp = data['main']['temp']
+    desc = data['weather'][0]['description']
+    print(f'Temperature: {temp} K')
+    print(f'Description: {desc}')
+else:
+    print('Error fetching weather data')
